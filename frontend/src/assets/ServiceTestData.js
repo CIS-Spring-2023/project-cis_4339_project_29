a811ddd5770eb2124edb4bb591a1679d16682820
DestonN <dnguyen122@uh.edu>
2023-03-13 02:45:59 -0500

    Added Test Data

---
+++ frontend/src/assets/ServiceTestData.js
@@ -0,0 +1,10 @@
+export const doughnutchartData = {
+  [
+    {name: "Service A", provName: "Tim Shock", active: true},
+    {name: "Helping People", provName: "Lighting Tony", active: false},
+    {name: "Tiny Dinos", provName: "Bob Cho", active: true},
+    {name: "Rocka Rolla", provName: "Tim Curr", active: false},
+    {name: "Tiny Kings", provName: "Sett Seed", active: true},
+  ]
+    };
+  export default doughnutchartData;
